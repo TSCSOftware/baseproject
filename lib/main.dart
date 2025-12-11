30850876ff94563b25ba62d536fa876439173e8d
Adnaan-codes <adnaansaleem514@gmail.com>
2025-12-11 19:04:54 +0530

    tst 2

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+ void bb1() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   void print_fx() {
     print("Hello World");
   }
